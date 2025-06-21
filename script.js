b67f3108ccc5d5c839c21e0b5f113f9c7db58da7
nolenphya <64279075+nolenphya@users.noreply.github.com>
2025-06-20 22:09:50 -0400

    06.20.25 v1.0

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ const map = new mapboxgl.Map({
 
 // Airtable Setup
 const AIRTABLE_API_KEY = 'patboskAQTJUi9FlQ.1c30c3c632cd4d7bd03cf949e50edd922425aba8dcbf0c8a6002e98db67c74a3';
-const BASE_ID = 'apph04Z2gcYOSx8Qm';
-const TABLE_NAME = 'tblr4J8Yw9bgmN26V';
+const BASE_ID = 'apppBx0a9hj0Z1ciw';
+const TABLE_NAME = 'tblgqyoE5TZUzQDKw';
 const AIRTABLE_URL = `https://api.airtable.com/v0/${BASE_ID}/${TABLE_NAME}`;
 
 // Globals
