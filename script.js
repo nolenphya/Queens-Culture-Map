d3bcf8d5e5d6e243a96c9181326b3f618df3b3cc
nolenphya <64279075+nolenphya@users.noreply.github.com>
2025-07-20 23:13:48 -0400

    07.20.25 v1.9

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiZmx1c2hpbmd0b3duaGFsbCIsImEiOiJjbWEzYmUzMWEwbnN3MmxwcjRyZG55ZmNxIn0.WRThoxFMtqTJQwV6Afv3ww';
 const map = new mapboxgl.Map({
   container: 'map',
- style: 'mapbox://styles/flushingtownhall/cmdci888o00ai01s2hdst2x3i',
+ style: 'https://api.mapbox.com/styles/v1/flushingtownhall/cmdci888o00ai01s2hdst2x3i.html?title=copy&access_token=pk.eyJ1IjoiZmx1c2hpbmd0b3duaGFsbCIsImEiOiJjbWEzYmUzMWEwbnN3MmxwcjRyZG55ZmNxIn0.WRThoxFMtqTJQwV6Afv3ww&zoomwheel=true&fresh=true#2/38/-34',
   center: [-73.94, 40.73],
   zoom: 11
 });
